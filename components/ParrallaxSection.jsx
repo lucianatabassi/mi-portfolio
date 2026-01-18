b07d0018a089756154f53fa48386af3bb66a5ab3
luciana <lutabassi@gmail.com>
2026-01-18 18:47:46 -0300

    mejoras 18/1

--- components/ParrallaxSection.jsx
+++ components/ParrallaxSection.jsx
@@ -7,9 +7,6 @@ function ParrallaxSection({ children }) {
         w-full 
         pt-10 pb-20 
         rounded-t-3xl
-        
-        /* CORRECCIÓN: Quitamos el margen gigante */
-        /* Ya no necesitamos mt-[...vh] porque la imagen de arriba ya ocupa ese espacio */
         mt-0
         
         /* OPCIONAL: Si quieres que el borde curvo muerda un poquito la foto, usa margen negativo */
